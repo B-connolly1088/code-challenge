a941e9a667d9c8cd7b71797fe024b1534e168202
Ben laptop <b.connolly1088@gmail.com>
2023-05-23 20:56:25 -0600

    added html, js, css

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+var startPage = document.querySelector("#start-page")
+var button = document.querySelector("#start-button")
+var totalScore = document.querySelector("#total-score")
+var qPage = document.querySelector("#q-page")
+
+var questions = [
+    {
+        question: "What is a method?",
+        choices: ["Actions that can be performed on objects.", "Actions that are performed on HTML.", "An element in JS."],
+        answer: 0
+    },
+    {
+        question: "What is a function responsible for in JS",
+        choices: ["Allows us to encapsulate a block of code to be reused.", "The basis of JS.", "Not used in JS."],
+        answer: 0
+    },
+    {
+        question: "What are event listeners in JS?",
+        choices: ["A way to wait for user interaction and then execute code upon event.", "How you load JS into your page.", "The last element used in building JS code."],
+        answer: 0
+    },
+]
+
+var currentQ = 0
+
+function buttonClicked(event) {
+    event.preventDefault();
+    startPage.classList.add("hide");
+    qPage.classList.remove("hide");
+    questionSelect();
+}
+
+function questionSelect() {
+    var q = questions[currentQ]
+    console.log(q);
+    var h2 = document.querySelector("#quest");
+    h2.textContent = q.question
+    for (var i = 0; i < q.choices.length; i++) {
+        var btn = document.getElementById(i)
+        btn.textContent = q.choices[i]
+        btn.addEventListener("click", answerSelect)
+    }
+}
+
+function answerSelect(event) {
+    event.preventDefault();
+    var userInput = event.target.id
+    var currentQuestion = questions[currentQ];
+    if (parseInt(userInput) === currentQuestion.answer) {
+        currentQ++;
+        if (currentQ < questions.length) {
+            questionSelect();
+        } else {
+            console.log("You're Done!");
+    }
+    }   
+}
+
+
+button.addEventListener("click", buttonClicked);
+
+
+
